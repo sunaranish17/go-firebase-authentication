5028e42644a30e5b98077395793a7d9ab21fb9f7
Anish <sunaranish17@gmail.com>
2021-01-29 15:39:02 +0545

    created firebase load file

---
+++ config/firebase.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"context"
+	"path/filepath"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"google.golang.org/api/option"
+)
+
+func SetupFirebase() *auth.Client {
+	serviceAccountKeyFilePath, err := filepath.Abs("./serviceAccountKey.js")
+	if err != nil {
+		panic("Unable to load serviceAccountKey.json file")
+	}
+
+	opt := option.WithCredentialsFile(serviceAccountKeyFilePath)
+
+	//Firebase adm SDK initalization
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		panic("Firebase load error")
+	}
+
+	//Firebase Auth
+	auth, err := app.Auth(context.Background())
+	if err != nil {
+		panic("Firebase load error")
+	}
+
+	return auth
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module gofirebase
 go 1.15
 
 require (
-	firebase.google.com/go v3.13.0+incompatible // indirect
+	firebase.google.com/go v3.13.0+incompatible
 	firebase.google.com/go/v4 v4.2.0 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	google.golang.org/api v0.17.0
 	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
 )
